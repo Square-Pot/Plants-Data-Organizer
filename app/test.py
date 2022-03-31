37ea5bf80e10ef2426eca6521927639c56827c0b
Dmitry <d.natxa@gmail.com>
2022-03-31 19:33:04 +0300

    try opencv inRange to detect DM

--- app/test.py
+++ app/test.py
@@ -1,3 +1,7 @@
+# https://stackoverflow.com/questions/69961871/how-to-compute-mean-and-standard-deviation-of-edges-canny-edge-detection-in-ope
+# https://answers.opencv.org/question/119161/detecting-not-decoding-datamatrix-rectangle-regions-in-an-image/
+
+from re import M
 import cv2
 import numpy as np
 
@@ -5,30 +9,61 @@ image = cv2.imread(r"/home/dntx/win/reps/DataMatrix-Sorter/app/IMG_20220330_0757
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 blur = cv2.medianBlur(gray, 5)
-sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
-sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
 
-thresh = cv2.threshold(sharpen,160,255, cv2.THRESH_BINARY_INV)[1]
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
 
-cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+mean, std = cv2.meanStdDev(blur)
 
-min_area = 100
-max_area = 1500
-image_number = 0
-for c in cnts:
-    area = cv2.contourArea(c)
-    if area > min_area and area < max_area:
-        x,y,w,h = cv2.boundingRect(c)
-        ROI = image[y:y+h, x:x+w]
-        cv2.imwrite('ROI_{}.png'.format(image_number), ROI)
-        cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
-        image_number += 1
+print(mean, std)
 
-cv2.imshow('sharpen', sharpen)
-cv2.imshow('close', close)
+x = 1.7
+
+tr_1 = mean - x*std
+tr_2 = mean + x*std
+
+
+# threshold on background color
+thresh = cv2.inRange(blur, tr_1, tr_2)
+
+
+# sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
+# sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
+
+# thresh = cv2.threshold(sharpen,220,255, cv2.THRESH_BINARY_INV)[1]
+# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+# close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+
+# mser = cv2.MSER_create()
+# mser.setMinArea(50)
+# mser.setMaxArea(100)
+# regions, _ = mser.detectRegions(close)
+
+# hulls = [cv2.convexHull(p.reshape(-1, 1, 2)) for p in regions]
+# cv2.polylines(image, hulls, 1, (0, 0, 255), 2)
+
+
+
+
+# cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+# print(cnts)
+
+# min_area = 100
+# max_area = 2500
+# image_number = 0
+# for c in cnts:
+#     area = cv2.contourArea(c)
+#     print(area)
+#     if area > min_area and area < max_area:
+#         x,y,w,h = cv2.boundingRect(c)
+#         # ROI = image[y:y+h, x:x+w]
+#         # cv2.imwrite('ROI_{}.png'.format(image_number), ROI)
+#         cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+        # image_number += 1
+
+# cv2.imshow('gray', gray)
+# cv2.imshow('blur', blur)
+# cv2.imshow('sharpen', sharpen)
 cv2.imshow('thresh', thresh)
-cv2.imshow('image', image)
 cv2.waitKey()
