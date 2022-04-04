9161b6e4b08f871adbd8c4fa22c6ee58a14bb762
Dmitry <d.natxa@gmail.com>
2022-04-04 19:16:59 +0300

    in process

--- app/test.py
+++ app/test.py
@@ -24,6 +24,29 @@ tr_2 = mean + x*std
 # threshold on background color
 thresh = cv2.inRange(blur, tr_1, tr_2)
 
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
+#print(cnts)
+
+min_area = 100
+max_area = 2500
+image_number = 0
+for c in cnts:
+    area = cv2.contourArea(c)
+    #print(area)
+    #if area > min_area and area < max_area:
+    x,y,w,h = cv2.boundingRect(c)
+        # ROI = image[y:y+h, x:x+w]
+        # cv2.imwrite('ROI_{}.png'.format(image_number), ROI)
+    cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+    image_number += 1
+
+
+
 
 # sharpen_kernel = np.array([[-1,-1,-1], [-1,9,-1], [-1,-1,-1]])
 # sharpen = cv2.filter2D(blur, -1, sharpen_kernel)
@@ -65,5 +88,5 @@ thresh = cv2.inRange(blur, tr_1, tr_2)
 # cv2.imshow('gray', gray)
 # cv2.imshow('blur', blur)
 # cv2.imshow('sharpen', sharpen)
-cv2.imshow('thresh', thresh)
+cv2.imshow('thresh', image)
 cv2.waitKey()
