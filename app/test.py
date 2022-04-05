05eecb7d113afb7e550033a9788124130546a6f0
Dmitry <d.natxa@gmail.com>
2022-04-05 19:35:50 +0300

    add: liks

--- app/test.py
+++ app/test.py
@@ -1,5 +1,8 @@
 # https://stackoverflow.com/questions/69961871/how-to-compute-mean-and-standard-deviation-of-edges-canny-edge-detection-in-ope
 # https://answers.opencv.org/question/119161/detecting-not-decoding-datamatrix-rectangle-regions-in-an-image/
+# http://note.sonots.com/SciSoftware/haartraining.html
+#https://stackoverflow.com/questions/29365813/opencv-2d-barcode-data-matrix-detection
+#https://github.com/egonSchiele/OpenCV/blob/master/samples/python/squares.py
 
 from re import M
 import cv2
@@ -8,42 +11,66 @@ import numpy as np
 image = cv2.imread(r"/home/dntx/win/reps/DataMatrix-Sorter/app/IMG_20220330_0757191_50p.jpg")
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-blur = cv2.medianBlur(gray, 5)
 
 
-mean, std = cv2.meanStdDev(blur)
+qrCodeDetector = cv2.findDataMatrix(gray)
+qr = qrCodeDetector.detect(image)
 
-print(mean, std)
-
-x = 1.7
-
-tr_1 = mean - x*std
-tr_2 = mean + x*std
+print(qr)
 
 
-# threshold on background color
-thresh = cv2.inRange(blur, tr_1, tr_2)
+# blur = cv2.medianBlur(gray, 5)
 
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
-close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
 
-cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+# mean, std = cv2.meanStdDev(blur)
+
+# #print(mean, std)
+
+# x = 1.7
+
+# tr_1 = mean - x*std
+# tr_2 = mean + x*std
+
+
+# # threshold on background color
+# thresh = cv2.inRange(blur, tr_1, tr_2)
+
+# kernel = cv2.getStructuringElement(cv2.MORPH_RECT, (3,3))
+# close = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel, iterations=2)
+
+# blur = cv2.blur(close, (3, 3))
+
+
+
+
+# edges = cv2.Canny(blur,100,200)
+
+
+# cv2.imshow('thresh', edges)
+# cv2.waitKey()
+
+
+
+
+
+# cnts = cv2.findContours(close, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+# cnts = cnts[0] if len(cnts) == 2 else cnts[1]
+
 
 #print(cnts)
 
-min_area = 100
-max_area = 2500
-image_number = 0
-for c in cnts:
-    area = cv2.contourArea(c)
-    #print(area)
-    #if area > min_area and area < max_area:
-    x,y,w,h = cv2.boundingRect(c)
-        # ROI = image[y:y+h, x:x+w]
-        # cv2.imwrite('ROI_{}.png'.format(image_number), ROI)
-    cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
-    image_number += 1
+# min_area = 100
+# max_area = 2500
+# image_number = 0
+# for c in cnts:
+#     area = cv2.contourArea(c)
+#     #print(area)
+#     #if area > min_area and area < max_area:
+#     x,y,w,h = cv2.boundingRect(c)
+#         # ROI = image[y:y+h, x:x+w]
+#         # cv2.imwrite('ROI_{}.png'.format(image_number), ROI)
+#     cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 2)
+#     image_number += 1
 
 
 
@@ -88,5 +115,5 @@ for c in cnts:
 # cv2.imshow('gray', gray)
 # cv2.imshow('blur', blur)
 # cv2.imshow('sharpen', sharpen)
-cv2.imshow('thresh', image)
-cv2.waitKey()
+# cv2.imshow('thresh', edges)
+# cv2.waitKey()
