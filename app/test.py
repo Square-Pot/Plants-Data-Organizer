ac8af0c2cf46124bba21cfb1d98f0883b3719914
Dmitry <d.natxa@gmail.com>
2022-04-06 19:24:03 +0300

    bounding rects found

--- app/test.py
+++ app/test.py
@@ -4,19 +4,77 @@
 #https://stackoverflow.com/questions/29365813/opencv-2d-barcode-data-matrix-detection
 #https://github.com/egonSchiele/OpenCV/blob/master/samples/python/squares.py
 
-from re import M
 import cv2
+from cv2 import CV_32SC2
+from cv2 import CV_32S
 import numpy as np
+from utils import image_resize
+
+
+
 
 image = cv2.imread(r"/home/dntx/win/reps/DataMatrix-Sorter/app/IMG_20220330_0757191_50p.jpg")
 
+image = image_resize(image, height=500)
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+#blur = cv2.medianBlur(gray, 5)
+
+mean, std = cv2.meanStdDev(gray)
+x = 1.8
+tr_1 = mean - x*std
+tr_2 = mean + x*std
+
+thresh = cv2.inRange(gray, tr_1, tr_2)
+
+contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
+# calculate min area of data matrix
+full_area = image.shape[0] * image.shape[1]
+dm_area = full_area / 200
+
+# get boundle rectanles
+for c in contours: 
+        area = cv2.contourArea(c)
+        if area > dm_area and area < full_area - 1000:
+                cv2.drawContours(image, [c], 0, (0,0,255), 2)
+                x,y,w,h = cv2.boundingRect(c)
+                cv2.rectangle(image, (x, y), (x + w, y + h), (36,255,12), 1)
+
+# get masks according to rectangles
+
+# check histogramms for this areas
+
+# if histogramm is similar for datamatrix, return this part of image
 
 
-qrCodeDetector = cv2.findDataMatrix(gray)
-qr = qrCodeDetector.detect(image)
 
-print(qr)
+
+cv2.imshow('thresh', thresh)
+cv2.imshow('hsv', image)
+cv2.waitKey()
+
+
+
+
+# harris = cv2.cornerHarris(gray, 2, 5, 0.04) 
+
+# _, thr = cv2.threshold(harris, 0.001 * harris.max(), 255, cv2.THRESH_BINARY)
+# thr = thr.astype('uint8')
+
+# contours, _ = cv2.findContours(thr, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+# contours = list(filter(lambda x: cv2.contourArea(cv2.convexHull(x)) > 1000, contours))
+
+# #print(contours)
+
+# for i, contour in enumerate(contours):
+#         mask = np.zeros_like(image)
+#         cv2.drawContours(gray, contour, -1, (255, 0, 0), -1)
+
+
+
+
+
+
 
 
 # blur = cv2.medianBlur(gray, 5)
